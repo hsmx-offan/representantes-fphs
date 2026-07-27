fb9ffc88f38a0ca834d89fab1eb4cbf566fab72d
hsmx-offan <hsmx.offan@gmail.com>
2026-07-27 11:35:33 -0600

    Update panel.js

--- admin/panel.js
+++ admin/panel.js
@@ -25,7 +25,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
+const db = getFirestore(app);
 
 // ========================================
 // ELEMENTOS
