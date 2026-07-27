e0af5849233699735886d17cae6c91b524d0fc26
hsmx-offan <hsmx.offan@gmail.com>
2026-07-26 19:28:49 -0600

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,24 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
 import {
   getAuth,
   signInWithEmailAndPassword,
   onAuthStateChanged,
   signOut
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyAesU9F4Oc7Lr8TPOFUk-Oi-lT086XjRKw",
-    authDomain: "hsmx-representantes.firebaseapp.com",
-    projectId: "hsmx-representantes",
-    storageBucket: "hsmx-representantes.firebasestorage.app",
-    messagingSenderId: "821385801252",
-    appId: "1:821385801252:web:c95ba9ffdeb90fe03732b1"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAesU9F4Oc7Lr8TPOFUk-Oi-lT086XjRKw",
+  authDomain: "hsmx-representantes.firebaseapp.com",
+  projectId: "hsmx-representantes",
+  storageBucket: "hsmx-representantes.firebasestorage.app",
+  messagingSenderId: "821385801252",
+  appId: "1:821385801252:web:c95ba9ffdeb90fe03732b1"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -28,12 +24,13 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const errorMessage = document.getElementById("errorMessage");
 
-loginForm?.addEventListener("submit", async (event) => {
+loginForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   errorMessage.textContent = "";
 
   try {
+
     await signInWithEmailAndPassword(
       auth,
       emailInput.value.trim(),
@@ -41,19 +38,29 @@ loginForm?.addEventListener("submit", async (event) => {
     );
 
     window.location.href = "panel.html";
+
   } catch (error) {
-    console.error(error);
-    errorMessage.textContent = "Correo o contraseña incorrectos.";
+
+    console.error("Error de inicio de sesión:", error);
+
+    errorMessage.textContent =
+      "Correo o contraseña incorrectos.";
+
   }
 });
 
 onAuthStateChanged(auth, (user) => {
-  if (user && window.location.pathname.endsWith("/admin/")) {
+
+  if (user) {
     window.location.href = "panel.html";
   }
+
 });
 
 window.logoutAdmin = async function () {
+
   await signOut(auth);
+
   window.location.href = "./";
+
 };
