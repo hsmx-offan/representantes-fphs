b2016fdc5952f4545c9d386f7f8a1ec5815a1386
hsmx-offan <hsmx.offan@gmail.com>
2026-07-28 04:32:29 -0600

    Update qr.js

--- admin/js/panel/qr.js
+++ admin/js/panel/qr.js
@@ -1,1 +1,24 @@
+export const REPRESENTANTE_URL =
+  "https://hsmx-offan.github.io/representantes-fphs/representante.html?id=";
 
+export function generarQR(qrContainer, idRepresentante) {
+
+  qrContainer.innerHTML = "";
+
+  new QRCode(qrContainer, {
+
+    text:
+      REPRESENTANTE_URL +
+      encodeURIComponent(idRepresentante),
+
+    width: 260,
+    height: 260,
+
+    colorDark: "#ffffff",
+    colorLight: "#000000",
+
+    correctLevel: QRCode.CorrectLevel.H
+
+  });
+
+}
