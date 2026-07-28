d5702bd7117f1a2c80441b8420c59553d7e97ac5
hsmx-offan <hsmx.offan@gmail.com>
2026-07-28 02:09:55 -0600

    Update index.js

--- admin/js/dashboard/index.js
+++ admin/js/dashboard/index.js
@@ -1,15 +1,9 @@
 import {
-  initializeApp
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-
-import {
-  getAuth,
   onAuthStateChanged,
   signOut
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
 import {
-  getFirestore,
   doc,
   getDoc,
   collection,
@@ -18,28 +12,10 @@ import {
   where
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-
-// ========================================
-// FIREBASE
-// ========================================
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAesU9F4Oc7Lr8TPOFUk-Oi-lT086XjRKw",
-  authDomain: "hsmx-representantes.firebaseapp.com",
-  projectId: "hsmx-representantes",
-  storageBucket: "hsmx-representantes.firebasestorage.app",
-  messagingSenderId: "821385801252",
-  appId: "1:821385801252:web:c95ba9ffdeb90fe03732b1"
-};
-
-const app =
-  initializeApp(firebaseConfig);
-
-const auth =
-  getAuth(app);
-
-const db =
-  getFirestore(app);
+import {
+  auth,
+  db
+} from "../shared/firebase.js";
 
 
 // ========================================
