27dcd54d7f242f36ccc1a028508260811dbd1b72
hsmx-offan <hsmx.offan@gmail.com>
2026-07-28 04:02:12 -0600

    Update perfil.js

--- admin/js/dashboard/perfil.js
+++ admin/js/dashboard/perfil.js
@@ -1,1 +1,57 @@
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
+import {
+  db
+} from "../shared/firebase.js";
+
+
+// ========================================
+// CARGAR PERFIL DEL ADMIN
+// ========================================
+
+export async function cargarPerfilAdmin(user) {
+
+  try {
+
+    const referencia =
+      doc(
+        db,
+        "admins",
+        user.uid
+      );
+
+    const documento =
+      await getDoc(
+        referencia
+      );
+
+    if (
+      documento.exists()
+    ) {
+
+      return (
+        documento.data().usuario ||
+        "Admin"
+      );
+
+    }
+
+    return "Admin";
+
+  }
+
+  catch (error) {
+
+    console.error(
+      "Error cargando perfil del admin:",
+      error
+    );
+
+    return "Admin";
+
+  }
+
+}
