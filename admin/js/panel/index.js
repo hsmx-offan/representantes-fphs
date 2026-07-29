58ba89704a3d85333f4f399b64b33bf44e126b55
hsmx-offan <hsmx.offan@gmail.com>
2026-07-28 20:54:52 -0600

    Update index.js

--- admin/js/panel/index.js
+++ admin/js/panel/index.js
@@ -40,8 +40,8 @@ const cargando =
 const contenido =
   document.getElementById("contenido");
 
-const nombreAdmin:
-  document.getElementById("nombreAdmin"),
+const nombreAdmin =
+  document.getElementById("nombreAdmin");
     
 const logoutButton =
   document.getElementById("logoutButton");
