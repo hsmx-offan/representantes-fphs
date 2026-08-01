1030981254f48a2e5773aaa6d82cb86ddb5bd08b
hsmx-offan <hsmx.offan@gmail.com>
2026-08-01 09:32:09 -0600

    Update recuerdos.js

--- js/recuerdos.js
+++ js/recuerdos.js
@@ -1,1 +1,74 @@
+import {
+  db
+} from "../admin/js/shared/firebase.js";
 
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+
+const grid =
+  document.getElementById(
+    "gridRecuerdos"
+  );
+
+
+cargarRecuerdos();
+
+
+async function cargarRecuerdos() {
+
+  try {
+
+    const consulta =
+      query(
+
+        collection(
+          db,
+          "recuerdos"
+        ),
+
+        where(
+          "estado",
+          "==",
+          "aprobado"
+        ),
+
+        orderBy(
+          "fechaCreacion",
+          "desc"
+        )
+
+      );
+
+    const snapshot =
+      await getDocs(
+        consulta
+      );
+
+    grid.innerHTML = "";
+
+    snapshot.forEach(doc => {
+
+      const recuerdo =
+        doc.data();
+
+      crearTarjeta(
+        recuerdo
+      );
+
+    });
+
+  }
+
+  catch(error){
+
+    console.error(error);
+
+  }
+
+}
