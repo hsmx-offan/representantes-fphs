77c6c481dd1688bf5932c459298c916eaa3f8906
hsmx-offan <hsmx.offan@gmail.com>
2026-08-03 21:40:55 -0600

    Create color-manager.js

---
+++ admin/js/color-manager.js
@@ -0,0 +1,71 @@
+const eventosDemo=[
+
+{
+
+id:"2026",
+
+nombre:"Together Together Tour México 2026",
+
+activo:true
+
+},
+
+{
+
+id:"2028",
+
+nombre:"Harry Styles México 2028",
+
+activo:false
+
+}
+
+];
+
+const lista=document.getElementById("listaEventos");
+
+const activo=document.getElementById("eventoActivo");
+
+function cargarEventos(){
+
+const eventoActivo=eventosDemo.find(e=>e.activo);
+
+activo.textContent=eventoActivo.nombre;
+
+lista.innerHTML="";
+
+eventosDemo.forEach(evento=>{
+
+lista.innerHTML+=`
+
+<div class="evento">
+
+<div>
+
+<strong>${evento.nombre}</strong>
+
+<br>
+
+<small>
+
+${evento.activo?"🟢 Activo":"⚪ Inactivo"}
+
+</small>
+
+</div>
+
+<button>
+
+Abrir
+
+</button>
+
+</div>
+
+`;
+
+});
+
+}
+
+cargarEventos();
