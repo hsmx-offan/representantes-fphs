c50542b6788777af0dc3e1761b1b879e43e66827
hsmx-offan <hsmx.offan@gmail.com>
2026-08-04 05:39:14 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,148 @@
+const CACHE_NAME = "mi-color-v1";
+
+
+const ARCHIVOS = [
+
+  "/representantes-fphs/",
+
+  "/representantes-fphs/mi-color.html",
+
+  "/representantes-fphs/css/mi-color.css",
+
+  "/representantes-fphs/js/mi-color/index.js",
+
+  "/representantes-fphs/js/mi-color/api.js",
+
+  "/representantes-fphs/js/mi-color/render.js",
+
+  "/representantes-fphs/manifest.webmanifest",
+
+  "/representantes-fphs/img/icon-192.png",
+
+  "/representantes-fphs/img/icon-512.png",
+
+  "/representantes-fphs/admin/js/shared/firebase.js",
+
+  "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js",
+
+  "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js",
+
+  "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js"
+
+];
+
+
+
+self.addEventListener(
+
+  "install",
+
+  event => {
+
+    event.waitUntil(
+
+      caches.open(
+
+        CACHE_NAME
+
+      ).then(
+
+        cache =>
+
+          cache.addAll(
+
+            ARCHIVOS
+
+          )
+
+      )
+
+    );
+
+  }
+
+);
+
+
+
+self.addEventListener(
+
+  "activate",
+
+  event => {
+
+    event.waitUntil(
+
+      caches.keys().then(
+
+        keys =>
+
+          Promise.all(
+
+            keys.map(
+
+              key => {
+
+                if (
+
+                  key !==
+
+                  CACHE_NAME
+
+                ) {
+
+                  return caches.delete(
+
+                    key
+
+                  );
+
+                }
+
+              }
+
+            )
+
+          )
+
+      )
+
+    );
+
+  }
+
+);
+
+
+
+self.addEventListener(
+
+  "fetch",
+
+  event => {
+
+    event.respondWith(
+
+      caches.match(
+
+        event.request
+
+      ).then(
+
+        respuesta =>
+
+          respuesta ||
+
+          fetch(
+
+            event.request
+
+          )
+
+      )
+
+    );
+
+  }
+
+);
